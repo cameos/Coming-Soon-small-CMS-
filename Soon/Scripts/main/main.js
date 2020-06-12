9bc6c24ad80c5d467c917c8a7159f1eb8d225781
Bongani Qunta <magine20@gmail.com>
2020-06-12 16:54:29 +0200

    config

--- Soon/Scripts/main/main.js
+++ Soon/Scripts/main/main.js
@@ -1,1 +1,18 @@
-﻿
+﻿var $ = jQuery.noConflict();
+$(document).ready(function () {
+    var image = new Array('../Content/images/anonymous-black-person-with-large-plaid-bag-standing-by-blue-4177747.jpg', '../Content/images/close-up-of-shoes-and-bag-336372.jpg', '../Content/images/pexels-4599047.jpg', '../Content/images/photo-of-woman-wearing-head-scarf-2951093.jpg', '../Content/images/photo-of-man-standing-beside-doorway-2951458.jpg', '../Content/images/serious-black-woman-standing-with-storage-bag-4177839.jpg', '../Content/images/photo-of-woman-touching-her-jaw-2951092.jpg'); 
+    var nextimage = 0;
+    doSlideShow();
+
+    function doSlideShow()
+    {
+        if (nextimage >= image.length)
+            nextimage = 0;
+        $('.page-wrap')
+            .css('background-image', 'url(" ' + image[nextimage++] + ' ")')
+            .fadeIn(500,function () {
+                setTimeout(doSlideShow, 4000);
+            });
+    }
+
+});
